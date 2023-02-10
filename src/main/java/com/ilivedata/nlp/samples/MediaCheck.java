a6b51cea512ab91c0b9ca412e6d87ad17a91987c
jianjun.zhao <37098112+jianjunzhao-github@users.noreply.github.com>
2023-02-10 20:53:00 +0800

    Update MediaCheck.java

--- src/main/java/com/ilivedata/nlp/samples/MediaCheck.java
+++ src/main/java/com/ilivedata/nlp/samples/MediaCheck.java
@@ -71,21 +71,6 @@ public class MergeCheck {
         return response.body().string();
     }
 
-    public static String http_build_query(Map<String,Object> array){
-        String reString = "";
-        Iterator it = array.entrySet().iterator();
-        while (it.hasNext()){
-            Map.Entry<String,Object> entry =(Map.Entry) it.next();
-            String key = entry.getKey();
-            Object value = entry.getValue();
-            reString += key+"="+value+"&";
-        }
-        reString = reString.substring(0, reString.length()-1);
-        reString = java.net.URLEncoder.encode(reString);
-        reString = reString.replace("%3D", "=").replace("%26", "&");
-        return reString;
-    }
-
     public static void main(String[] args) throws IOException {
         MergeCheck mergeCheck = new MergeCheck();
         List<String> imageList = new ArrayList<>();
